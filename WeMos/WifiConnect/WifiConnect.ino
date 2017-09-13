fe6c8aa29f25ecca98829e095dcf81265e7cb6e3
Mayank Chaudhary <mayankc7h7@gmail.com>
2017-09-12 19:55:03 -0500

    add serial outputs, fixed high low

--- WeMos/WifiConnect/WifiConnect.ino
+++ WeMos/WifiConnect/WifiConnect.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
  
-const char* ssid = "ssid name";
-const char* password = "ssid password";
+const char* ssid = "";
+const char* password = "";
  
 WiFiServer server(80);
  
@@ -14,27 +14,15 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
  
   // Connect to WiFi network
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
- 
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println("");
-  Serial.println("WiFi connected");
  
   // Start the server
   server.begin();
-  Serial.println("Server started");
- 
-  // Print the IP address
-  Serial.print("Use this URL : ");
-  Serial.print("http://");
   Serial.print(WiFi.localIP());
   Serial.println("/");
  
@@ -48,39 +36,25 @@ void loop() {
   }
  
   // Wait until the client sends some data
-  Serial.println("new client");
   while(!client.available()){
     delay(1);
   }
  
   // Read the first line of the request
   String request = client.readStringUntil('\r');
-  Serial.println(request);
   client.flush();
  
   // Match the request
  
   int value = LOW;
   if (request.indexOf("/LED=ON") != -1) {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_BUILTIN, LOW);
     value = HIGH;
   } 
   if (request.indexOf("/LED=OFF") != -1){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_BUILTIN, HIGH);
     value = LOW;
   }
- 
- 
- 
-  // Return the response
-  client.println("HTTP/1.1 200 OK");
-  client.println("Content-Type: text/html");
-  client.println(""); //  do not forget this one
-  client.println("<!DOCTYPE HTML>");
-  client.println("<html>");
- 
-  client.print("Led is now: ");
- 
   if(value == HIGH) {
     client.print("On");  
   } else {
@@ -88,8 +62,6 @@ void loop() {
   }
  
   delay(1);
-  Serial.println("Client disconnected");
-  Serial.println("");
  
 }
 
